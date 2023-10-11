e84535f16ec69d5738c2c90db6526e2f687db65d
ProAlize <105857567+ProAlize@users.noreply.github.com>
2023-10-11 13:50:25 +0800

    build: 装装need依赖

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Pinia
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(pinia)
+app.mount('#app')
